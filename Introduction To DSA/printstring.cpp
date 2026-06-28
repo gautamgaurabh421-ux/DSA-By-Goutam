8f161066bfc72de4585635d314cdd332de30c860
gautam gaurabh <gautamgaurabh421@gmail.com>
2026-06-28 07:56:38 +0530

    adding elements

--- Introduction To DSA/printstring.cpp
+++ Introduction To DSA/printstring.cpp
@@ -1,11 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 int main()
 {
-    int x, y;
-    cin >> x;
-    cin >> y;
-    cout << x + y;
+    string s;
+    getline(cin, s);
+    cout << s << endl;
     return 0;
 }
